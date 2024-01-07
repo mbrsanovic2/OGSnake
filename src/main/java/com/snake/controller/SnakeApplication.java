d8d2d4bc8082a42a344fa55c4645b54e83323ed1
mbrsanovic <146012622+mbrsanovic2@users.noreply.github.com>
2024-01-07 15:34:28 +0100

    Adding Main Menu

--- src/main/java/com/snake/controller/SnakeApplication.java
+++ src/main/java/com/snake/controller/SnakeApplication.java
@@ -3,17 +3,26 @@ package com.snake.controller;
 import com.snake.model.GameStep;
 import com.snake.view.GameBoard;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
  * Defines the basic Things we need at the Start of our Application (View, Model, Controller)
  */
 public class SnakeApplication extends Application {
+    private Stage stage;
     /**
      * Our Pseudo Main Method generates all starter Objects and Sets them
      *
@@ -21,20 +30,69 @@ public class SnakeApplication extends Application {
      */
     @Override
     public void start(Stage stage) {
-
-        // Create a Canvas to Paint things on
-        Pane canvas = new Pane();
-
-        // Create the window with the specified size and canvas
-        Scene scene = new Scene(canvas, 900, 600);
-        // Cannot resize Window
+        this.stage = stage;
 
         // Set up the stage
         stage.setTitle("Snake");
-        stage.setScene(scene);
+        switchToMainMenuScene();
         stage.show();
+    }
+
+    private void switchToMainMenuScene() {
+        StackPane stackLayout = new StackPane();
+        stackLayout.setBackground(new Background(getBackground()));
+
+        // Replace current Scene with MainMenuScene
+        stage.setScene(new Scene(stackLayout, 500, 400));
+
+        // Create Button and Text
+        Button switchToGameplay = new Button("Play!");
+        Text text = new Text("SNAKE");
+        text.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+
+        // Change the Style of the Button
+        switchToGameplay.setStyle("-fx-background-color: #3498db;");
+        switchToGameplay.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        switchToGameplay.setMinSize(120, 60);
+
+        // Add Button functionality -> OnClick execute switchToGameplayScene()
+        switchToGameplay.setOnAction(e -> switchToGameplayScene());
+
+        // Add to Pane so that it is now in our Scene
+        stackLayout.getChildren().addAll(text, switchToGameplay);
+        StackPane.setAlignment(text, Pos.TOP_CENTER);
+        StackPane.setAlignment(switchToGameplay, Pos.TOP_CENTER);
+        StackPane.setMargin(text, new Insets(22));
+        StackPane.setMargin(switchToGameplay, new Insets(130));
+    }
+
+    private static BackgroundImage getBackground() {
+        Image backgroundImage = new Image("snake.png");
+        BackgroundImage background = new BackgroundImage(
+                backgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)
+        );
+
+        return background;
+    }
+
+    private void switchToGameplayScene() {
+        // Create a Canvas to Paint things on
+        Pane canvas = new Pane();
+        // Create the window with the specified size and canvas
+        Scene scene = new Scene(canvas, 900, 600);
+        stage.setScene(scene);
         GameBoard gameBoard = new GameBoard(canvas);
         GameStep gameStep = new GameStep(gameBoard);
+
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        double x = bounds.getMinX() + (bounds.getWidth() - scene.getWidth()) * 0.55;
+        double y = bounds.getMinY() + (bounds.getHeight() - scene.getHeight()) * 0.5;
+        stage.setX(x);
+        stage.setY(y);
 
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
             // assign direction to keyboard and prevent reverse movement
